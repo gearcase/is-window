cfdf84d5b8d9e333be9de26c909e8da1965a3552
bubkoo <bubkoo.wy@gmail.com>
2016-04-13 10:40:27 +0800

    test spec

--- test/spec/index.js
+++ test/spec/index.js
@@ -6,41 +6,25 @@ var expect = char.expect;
 
 describe('is-window', function () {
 
-    var isWindow = require('../../index');
+  var isWindow = require('../../index');
 
-    it('isWindow()', function () {
-        expect(isWindow()).to.be.false;
-    });
+  it('common test', function () {
+    expect(isWindow()).to.be.false;
 
-    it('isWindow(1)', function () {
-        expect(isWindow(1)).to.be.false;
-    });
+    expect(isWindow(1)).to.be.false;
 
-    it('isWindow("1")', function () {
-        expect(isWindow('1')).to.be.false;
-    });
+    expect(isWindow('1')).to.be.false;
 
-    it('isWindow(true)', function () {
-        expect(isWindow(true)).to.be.false;
-    });
+    expect(isWindow(true)).to.be.false;
 
-    it('isWindow(null)', function () {
-        expect(isWindow(null)).to.be.false;
-    });
+    expect(isWindow(null)).to.be.false;
 
-    it('isWindow({})', function () {
-        expect(isWindow({})).to.be.false;
-    });
+    expect(isWindow({})).to.be.false;
 
-    it('isWindow(function)', function () {
-        expect(isWindow(function () {})).to.be.false;
-    });
+    expect(isWindow(function () {})).to.be.false;
 
-    it('isWindow([])', function () {
-        expect(isWindow([])).to.be.false;
-    });
+    expect(isWindow([])).to.be.false;
 
-    it('isWindow(arguments)', function () {
-        expect(isWindow(arguments)).to.be.false;
-    });
+    expect(isWindow(arguments)).to.be.false;
+  });
 });
